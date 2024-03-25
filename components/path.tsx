98c0a4808c3f0d2d288c3d5b299b074a54ca35a4
ruwpy <ruwpy.who@gmail.com>
2024-03-25 13:20:24 +0100

    made conventional folder navigation

--- components/path.tsx
+++ components/path.tsx
@@ -1,17 +1,27 @@
 "use client";
 
+import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 
 export const Path = () => {
   const pathname = usePathname();
-  const router = useRouter();
+
+  const backLink = pathname.split("/").slice(0, -1).join("/");
 
   return (
-    <div className="h-[40px] flex items-center gap-[15px] w-full flex-grow border-b border-[var(--font-color)]">
-      <button className="h-full bg-black text-white px-[4px]" onClick={() => router.back()}>
+    <div className="h-[40px] flex items-center gap-[10px] pr-[50px] w-full flex-grow border-b border-[var(--font-color)]">
+      <Link
+        className="h-full inline-flex items-center bg-black text-white px-[4px]"
+        href={`${window.location.origin}${backLink}`}
+      >
         back
-      </button>
-      <span>home{pathname}</span>
+      </Link>
+      <span className="flex justify-end overflow-hidden">
+        <Link href={"/"}>home</Link>
+        {pathname.split("/").map((p) => (
+          <Link href={`${window.location.origin}${pathname.split(p)[0] + p}`}>{p}/</Link>
+        ))}
+      </span>
     </div>
   );
 };
