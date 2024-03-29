b028bae994ae7a2fc7ecef0b22b7896b09d7496f
ruwpy <ruwpy.who@gmail.com>
2024-03-29 20:10:40 +0100

    [feature] added ability to add members to the project

--- app/[projectId]/page.tsx
+++ app/[projectId]/page.tsx
@@ -1,9 +1,11 @@
+import { AddMemberButton } from "@/components/button-add-member";
 import { CreateFolderButton } from "@/components/button-create-folder";
 import { UploadImageButton } from "@/components/button-upload-image";
 import { FoldersNImagesList } from "@/components/list-folders-n-images";
 import { validateRequest } from "@/lib/auth";
 import { getAllFoldersFromProject } from "@/lib/db/handlers/folder";
 import { getAllImagesFromProject } from "@/lib/db/handlers/image";
+import { getProject } from "@/lib/db/handlers/project";
 import { redirect } from "next/navigation";
 
 const ProjectPage = async ({ params }: { params: { projectId: string } }) => {
@@ -13,11 +15,15 @@ const ProjectPage = async ({ params }: { params: { projectId: string } }) => {
 
   const images = await getAllImagesFromProject({ projectId: params.projectId });
   const folders = await getAllFoldersFromProject({ projectId: params.projectId });
+  const project = await getProject({ projectId: params.projectId });
 
   return (
     <>
       <div className="flex">
         <div className="flex w-full flex-col">
+          {project.ownerId === user.id && (
+            <AddMemberButton userId={user.id} projectId={params.projectId} />
+          )}
           <CreateFolderButton projectId={params.projectId}>create folder</CreateFolderButton>
           <UploadImageButton projectId={params.projectId} />
         </div>
--- lib/db/handlers/member.ts
+++ lib/db/handlers/member.ts
@@ -4,6 +4,8 @@ import { eq } from "drizzle-orm";
 import { db } from "..";
 import { project } from "../schema/project";
 import { member } from "../schema/member";
+import { user } from "../schema/user";
+import { getProject } from "./project";
 
 export const getAllProjectMembers = async ({ projectId }: { projectId: string }) => {
   const projectFromDb = await db
@@ -16,4 +18,31 @@ export const getAllProjectMembers = async ({ projectId }: { projectId: string })
     userId: p.member.userId,
     projectId: p.member.projectId,
   }));
+};
+
+export const addMemberToProject = async ({
+  memberEmail,
+  userId,
+  projectId,
+}: {
+  memberEmail: string;
+  userId: string;
+  projectId: string;
+}) => {
+  const projectFromDb = await getProject({ projectId });
+  if (projectFromDb.ownerId !== userId) return undefined;
+
+  const members = await getAllProjectMembers({ projectId });
+
+  const [userFromDb] = await db.select().from(user).where(eq(user.email, memberEmail));
+
+  const isAlreadyMember = members.filter((m) => m.userId === userFromDb.email);
+  if (isAlreadyMember) return undefined;
+
+  const newMember = await db
+    .insert(member)
+    .values({ projectId, userId: userFromDb.id })
+    .returning();
+
+  return newMember;
 };
